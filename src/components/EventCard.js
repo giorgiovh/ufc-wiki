20400491484c0d7f9ba6bb7945e22c2353d974cf
giorgiovh <jorgevonhoroch@gmail.com>
2023-03-21 19:05:57 -0400

    changed event card to one that doesnt have a SEE DETAILS button

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,10 +1,46 @@
+// import * as React from 'react';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import { useNavigate } from 'react-router-dom';
+
+// const moment = require('moment');
+
+// export default function EventCard({ event }) {
+//   const navigate = useNavigate()
+
+//   return (
+//     <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }}>
+//       <CardMedia
+//         component="img"
+//         alt="ufc logo"
+//         height="140"
+//         image={require('./../images/ufc_logo.png')}
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h5" component="div">
+//           {event.Name}
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//           {moment(event.Day).format('MMM Do, YYYY')}
+//         </Typography>
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small" onClick={() => navigate(`/events/${event.EventId}`)}>See Details</Button>
+//       </CardActions>
+//     </Card>
+//   );
+// }
+
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const moment = require('moment');
@@ -14,23 +50,22 @@ export default function EventCard({ event }) {
 
   return (
     <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={require('./../images/ufc_logo.png')}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {event.Name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {moment(event.Day).format('MMM Do, YYYY')}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={() => navigate(`/events/${event.EventId}`)}>See Details</Button>
-      </CardActions>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require('./../images/ufc_logo.png')}
+          alt="ufc logo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {event.Name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {moment(event.Day).format('MMM Do, YYYY')}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
