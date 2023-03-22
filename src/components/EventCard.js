19049ea16ee8902afc70e7a66d853d54fec6636f
giorgiovh <jorgevonhoroch@gmail.com>
2023-03-22 09:57:43 -0400

    added functionality to the event cards so that when user clicks the card, it takes them to the details page

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,40 +1,3 @@
-// import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import { useNavigate } from 'react-router-dom';
-
-// const moment = require('moment');
-
-// export default function EventCard({ event }) {
-//   const navigate = useNavigate()
-
-//   return (
-//     <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }}>
-//       <CardMedia
-//         component="img"
-//         alt="ufc logo"
-//         height="140"
-//         image={require('./../images/ufc_logo.png')}
-//       />
-//       <CardContent>
-//         <Typography gutterBottom variant="h5" component="div">
-//           {event.Name}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {moment(event.Day).format('MMM Do, YYYY')}
-//         </Typography>
-//       </CardContent>
-//       <CardActions>
-//         <Button size="small" onClick={() => navigate(`/events/${event.EventId}`)}>See Details</Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
-
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -49,7 +12,7 @@ export default function EventCard({ event }) {
   const navigate = useNavigate()
 
   return (
-    <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }}>
+    <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }} onClick={() => navigate(`/events/${event.EventId}`)}>
       <CardActionArea>
         <CardMedia
           component="img"
