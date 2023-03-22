1705d5cd440f71291e2f432f95a4979ada2a0602
giorgiovh <jorgevonhoroch@gmail.com>
2023-03-22 10:04:43 -0400

    added new card style for fighters

--- src/components/FighterCard.js
+++ src/components/FighterCard.js
@@ -1,34 +1,32 @@
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function FighterCard({ fighter }) {
   const navigate = useNavigate()
 
   return (
-    <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={require('./../images/mma_glove.png')}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {`${fighter.FirstName} ${fighter.LastName}`}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {fighter.Nickname && `"${fighter.Nickname}"`}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={() => navigate(`/fighters/${fighter.FighterId}`)}>See Details</Button>
-      </CardActions>
+    <Card sx={{ maxWidth: 345, marginTop: '20px', marginBottom: '20px' }} onClick={() => navigate(`/fighters/${fighter.FighterId}`)}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require('./../images/ufc_logo.png')}
+          alt="ufc logo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {`${fighter.FirstName} ${fighter.LastName}`}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {fighter.Nickname && `"${fighter.Nickname}"`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
