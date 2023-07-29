5aa030f13043af0233cd5330fc7dcee6da867249
giorgiovh <jorgevonhoroch@gmail.com>
2023-07-29 14:04:49 -0400

    fixed issue where events wouldn't show in upcoming events the day that the event is on

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, {
+  useEffect,
+  useState
+} from 'react'
 import axios from 'axios'
 import EventCard from '../../components/EventCard';
 
 export const Home = () => {
   const [events, setEvents] = useState([])
-  
+
   const currentYear = new Date().getFullYear();
 
   const getEvents = () => {
@@ -16,10 +19,13 @@ export const Home = () => {
     getEvents()
   }, [])
 
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // set time to start of day
+
   return (
     <div className='page'>
       <h2>Upcoming Events</h2>
-      {events && events.filter(event => new Date(event.Day) >= new Date()).map(event => {
+      {events && events.filter(event => new Date(event.Day) >= today).map(event => {
         return (
           <EventCard key={event.EventId} event={event}/>
         )
